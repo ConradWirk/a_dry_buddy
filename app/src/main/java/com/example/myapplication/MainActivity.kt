bd2208cd0c6680f09f32fb64b30e7e6d3789f467
Mateusz <mzawada2004@gmail.com>
2024-01-15 12:58:07 +0100

    Przerobienie przycisków powrotu aby nie startowały nowego activity Dodanie funkcjonalności przycisku zweryfikuj za pomocą flora incognita

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,26 +1,39 @@
 package com.example.myapplication
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val roslina1 = findViewById<Button>(R.id.Roslina1)
-        roslina1.setOnClickListener(View.OnClickListener {  val intent = Intent(this, Roslina1::class.java)
-            startActivity(intent)})
+        roslina1.setOnClickListener {
+            val intent = Intent(this, Roslina1::class.java)
+            startActivity(intent)
+        }
         val roslina2 = findViewById<Button>(R.id.Roslina2)
-        roslina2.setOnClickListener(View.OnClickListener {  val intent = Intent(this, Roslina1::class.java)
-            startActivity(intent)})
+        roslina2.setOnClickListener {
+            val intent = Intent(this, Roslina1::class.java)
+            startActivity(intent)
+        }
         val roslina3 = findViewById<Button>(R.id.Roslina3)
-        roslina3.setOnClickListener(View.OnClickListener {  val intent = Intent(this, Roslina1::class.java)
-            startActivity(intent)})
+        roslina3.setOnClickListener {
+            val intent = Intent(this, Roslina1::class.java)
+            startActivity(intent)
+        }
+        val FloraIncognita = findViewById<Button>(R.id.floraincognita)
+        FloraIncognita.setOnClickListener {
+            val launchIntent = packageManager.getLaunchIntentForPackage("com.floraincognita.app.floraincognita")
+            startActivity(launchIntent)
+        }
     }
 }
--- app/src/main/java/com/example/myapplication/Roslina1.kt
+++ app/src/main/java/com/example/myapplication/Roslina1.kt
@@ -1,25 +1,15 @@
-package com.example.myapplication;
+package com.example.myapplication
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
-public class Roslina1 extends AppCompatActivity {
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.roslina_opis);
-
-        Button powrot = (Button)findViewById(R.id.Powrot);
-        powrot.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(Roslina1.this, MainActivity.class));
-            }
-        });
+class Roslina1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.roslina_opis)
+        val powrot = findViewById<View>(R.id.Powrot) as Button
+        powrot.setOnClickListener { finish() }
     }
-
 }
